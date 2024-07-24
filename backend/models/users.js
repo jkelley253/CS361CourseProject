198e0614aaed58e1e3cccc1486243c13bce303ae
Jackson Kelley <Jackson.kelley94@gmail.com>
2024-07-23 20:05:24 -0700

    added title to users model updated user fields to be required added title to orgChart model

--- backend/models/users.js
+++ backend/models/users.js
@@ -6,11 +6,12 @@ const userSchema = new mongoose.Schema({ //create a new mongoose schema for the 
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    manager: { type: String },
-    team: { type: String },
+    manager: { type: String, required: true },
+    team: { type: String, required: true  },
     status: {type: String, default: 'active'},
     groups: [String], 
     apps: [String],
+    title: { type: String, required: true  },
 });
 
 module.exports = mongoose.model('User', userSchema); //export the model
